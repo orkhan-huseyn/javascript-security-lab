0ca42e85d049cf00e563e87ec70cb0283ac0b85c
Orkhan Huseynli <orkhan.huseyn@outlook.com>
2023-03-30 17:27:05 +0400

    Use cookie-parser's signed cookies

--- server.js
+++ server.js
@@ -2,15 +2,16 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 
 const { USERS, BALANCES } = require('./database');
+const COOKIE_SECRET = 'SUPER SECRET KEY';
 
 const app = express();
 
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
+app.use(cookieParser(COOKIE_SECRET));
 
 app.get('/', function (req, res) {
-  const username = req.cookies.username;
+  const username = req.signedCookies.username;
   if (!username) {
     res.redirect('/login');
   } else {
@@ -33,7 +34,7 @@ app.get('/logout', function (req, res) {
 app.post('/login', function (req, res) {
   const { username, password } = req.body;
   if (USERS[username] == password) {
-    res.cookie('username', username);
+    res.cookie('username', username, { signed: true });
     res.redirect('/');
   } else {
     res.send('Username or password is wrong!');
@@ -41,7 +42,7 @@ app.post('/login', function (req, res) {
 });
 
 app.post('/transfer', function (req, res) {
-  const username = req.cookies.username;
+  const username = req.signedCookies.username;
   const { to, amount } = req.body;
 
   BALANCES[username] -= Number(amount);
